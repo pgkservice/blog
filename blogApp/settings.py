984d6f5d093482aeb8706389d0bba764aaab1db7
vince <routta.com@gmail.com>
2023-01-24 11:04:53 +0200

    debug false

--- blogApp/settings.py
+++ blogApp/settings.py
@@ -28,7 +28,7 @@ with open("website_secrete_informations.json") as file:
 SECRET_KEY = SECRETS['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
