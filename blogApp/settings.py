215a5b6d22eb4d69c03e5c9e766ec7a795bb7fb7
vince <routta.com@gmail.com>
2023-01-24 10:58:28 +0200

    sqlite update

--- blogApp/settings.py
+++ blogApp/settings.py
@@ -90,21 +90,21 @@ WSGI_APPLICATION = 'blogApp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'blog app',
-        'USER': 'postgres',
-        'PASSWORD': SECRETS['DB_PASSWORD']
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'blog app',
+#         'USER': 'postgres',
+#         'PASSWORD': SECRETS['DB_PASSWORD']
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
