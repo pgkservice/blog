891b0f8b155bc7b0de845db0b0a6fcdb6c7d0f40
vince <routta.com@gmail.com>
2023-01-24 10:48:57 +0200

    settings update

--- blogApp/settings.py
+++ blogApp/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = SECRETS['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['www.vincentproblog.com', 'vincentproblog.com', 'vincentblog.com']
+ALLOWED_HOSTS = ['*']
 
 
 AUTH_USER_MODEL = "accounts.User"
