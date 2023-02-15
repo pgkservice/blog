60b7e25ddc9323521d8c50fbfdf9135d486e4712
pgkservice <125490560+pgkservice@users.noreply.github.com>
2023-02-15 22:23:54 +0200

    Update settings.py

--- blogApp/settings.py
+++ blogApp/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
-    'django.contrib.postgres',
+#     'django.contrib.postgres',
 
     'blog.apps.BlogConfig',
     'taggit',
